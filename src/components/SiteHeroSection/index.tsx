575ab553b3fd3052dc1409f014a1e826abd4092d
ebbryan <earlbryanburaay@gmail.com>
2025-05-16 11:19:46 +0800

    adjusted font size

--- src/components/SiteHeroSection/index.tsx
+++ src/components/SiteHeroSection/index.tsx
@@ -22,16 +22,16 @@ export const SiteHeroSection = () => {
     setIsLg(!isLG ? 220 : 390);
   }, [isLG]);
   return (
-    <section className="flex flex-col justify-center items-center h-screen">
-      <div className="flex flex-col-reverse lg:flex-row items-center gap-5 w-full h-screen justify-center">
+    <section className="flex flex-col justify-center items-center h-screen ">
+      <div className="flex flex-col-reverse lg:flex-row items-center gap-5 w-full h-screen justify-center mt-12 md:mt-0">
         <div className="flex flex-col items-center lg:items-end gap-4">
           <div className="flex flex-col items-center lg:items-end">
             <h1 className="hidden lg:block lg:-mb-4">Hi! I am</h1>
-            <h1 className="text-7xl font-black">
+            <h1 className="text-5xl md:text-7xl font-black">
               Earl <span className="text-orange-500">Bryan</span>
             </h1>
           </div>
-          <h1 className="text-2xl ">Junior Frontend Developer</h1>
+          <h1 className="text-lg md:text-2xl">Junior Frontend Developer</h1>
           {/* STACKS HERE */}
           <div className="w-full flex flex-col lg:items-end">
             <article className="flex flex-col items-center gap-3 w-full lg:flex-row">
